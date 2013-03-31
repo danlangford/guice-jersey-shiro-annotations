ce1ba5587b709e08fca2d3e9fadcb9e6c14f920d
Dan Langford <danlangford@gmail.com>
2013-03-31 10:45:53 -0600

    starting point: simple, no xml servlet (3.0)

---
+++ src/main/java/com/example/shirohelp/MyServlet.java
@@ -0,0 +1,24 @@
+package com.example.shirohelp;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class MyServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws IOException {
+		res.getWriter().write("Hello! this is a test");
+	}
+
+}
